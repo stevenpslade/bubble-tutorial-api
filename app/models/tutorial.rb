9f0f680a4060f2618a31ba5c5f91360e6e78c6a4
stevenpslade <stevenpslade@gmail.com>
2017-03-27 22:21:36 -0700

    tests and validations for Tutorial model

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,7 @@ class Tutorial < ApplicationRecord
   belongs_to :user
   belongs_to :site
   has_many :tutorial_items, dependent: :destroy
+
+  validates :name, presence: true
+  validates :active, exclusion: { in: [nil] }
 end
--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:user) }
+  it { should belong_to(:site) }
+  it { should have_many(:tutorial_items) }
+  # Validation test
+  it { should validate_presence_of(:name) }
+  it { should validate_exclusion_of(:active).in_array([nil]) }
 end
