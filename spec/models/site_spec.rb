90adc20ba0531c003809e53b16d42bc4fadb5a6d
stevenpslade <stevenpslade@gmail.com>
2017-03-27 21:57:56 -0700

    adding rspec tests for site model

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Site, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Site belongs_to User
+  it { should belong_to(:user) }
+  # ensure Site has 1:m relationship with the Tutorial model
+  it { should have_many(:tutorials) }
+  # Validation tests
+  # ensure columns url, site_code are present before saving
+  it { should validate_presence_of(:url) }
+  it { should validate_presence_of(:site_code) }
 end
