14e0c96a02cd16b5c51d2dfcd4b7f6f53c1694de
stevenpslade <stevenpslade@gmail.com>
2017-03-27 22:04:27 -0700

    adding tests and validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :tutorials, dependent: :destroy
   has_many :sites, dependent: :destroy
+
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User has many Sites
+  # ensure User has many Tutorials
+  it { should have_many(:sites).dependent(:destroy) }
+  it { should have_many(:tutorials).dependent(:destroy) }
+  # Validation test
+  # ensure user has an email
+  it { should validate_presence_of(:email) }
 end
