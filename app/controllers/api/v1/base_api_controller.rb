fa5de99420ff5ca3fce20c0ad5818f8724a2a21a
stevenpslade <stevenpslade@gmail.com>
2017-03-28 22:06:38 -0700

    adding rescue at api base controller for ActiveRecord::RecordNotFound

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,4 +1,15 @@
 module Api::V1
   class BaseApiController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_rescue
+
+    def record_not_found_rescue(exception)
+      logger.info("#{exception.class}: " + exception.message)
+      if Rails.env.production?
+        render json: { status: 404, errors: 'Not found' }, status: :not_found
+      else
+        render json: { message: exception, backtrace: exception.backtrace }, status: :not_found
+      end
+    end
+
   end
 end
