34cad939003548c3c84697565760b32b06579ca6
stevenpslade <stevenpslade@gmail.com>
2017-05-06 23:07:46 -0700

    create a small amount of seed data to use with testing script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(email: 'steven@example.com')
+site = Site.create(url: 'example.com', user_id: user.id, site_code: '123456')
+
+tutorials = Tutorial.create([
+    {name: 'Welcome Tutorial', active: true, page_url: 'example.com/welcome', skippable: false, show_steps: true, user_id: user.id, site_id: site.id},
+    {name: 'Unwelcome Tutorial', active: false, page_url: 'example.com/welcome', skippable: false, show_steps: true, user_id: user.id, site_id: site.id}
+  ])
+
+tutorial_items = TutorialItem.create([
+    {title: 'step1', content: 'welcome to this site.', active: true, order: 1, css_selector: '.welcome-banner', tutorial: tutorials.first},
+    {title: 'step2', content: 'click here to signup...', active: true, order: 2, css_selector: '#signup', tutorial: tutorials.first},
+    {title: 'step3', content: 'give us your info!', active: true, order: 3, css_selector: '#newsletter', tutorial: tutorials.first}
+  ])
