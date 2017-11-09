4505cb9c737346ba736ec13b2bb371e7d14988ff
stevenpslade <stevenpslade@gmail.com>
2017-11-08 22:06:05 -0800

    configuring rails to work with react-router

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,8 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
