f02bd482dc84ba5d8834a169cad5997f2b00f8e1
stevenpslade <stevenpslade@gmail.com>
2017-11-13 07:59:56 -0800

    removing example comments

--- client/src/Main.js
+++ client/src/Main.js
@@ -4,11 +4,7 @@ import SignUp from './views/SignUp'
 import Bubble from './Bubble'
 
 class Main extends Component {
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
+
   render() {
     return (
       <main>
