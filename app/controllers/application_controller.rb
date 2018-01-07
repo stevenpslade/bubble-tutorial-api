8a7decd7614e51e9d94c145826e4ab7cf3360928
stevenpslade <stevenpslade@gmail.com>
2018-01-06 22:11:52 -0800

    testing rendering of plain text only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-
-    f = File.open("public/index.html", "r")
-    f.each_line do |line|
-      puts line
-    end
-    f.close
-
-    render file: 'public/index.html'
+    # render file: 'public/index.html'
+    render plain: "OK"
   end
 end
