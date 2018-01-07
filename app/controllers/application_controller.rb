ff981579da9568bef3e6edb6904d1be047e535bb
stevenpslade <stevenpslade@gmail.com>
2018-01-06 21:57:27 -0800

    testing to see if index.html has content after build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    puts "hit fallback_index_html"
+
+    f = File.open("public/index.html", "r")
+    f.each_line do |line|
+      puts line
+    end
+    f.close
+
     render file: 'public/index.html'
   end
 end
