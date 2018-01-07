e674629c0d709273233df8e2c9218adb335e993a
stevenpslade <stevenpslade@gmail.com>
2018-01-06 22:36:34 -0800

    enabling serving static files from public on prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    render file: "public/index.html", content_type: "text/html"
+    render file: "public/index.html"
   end
 end
