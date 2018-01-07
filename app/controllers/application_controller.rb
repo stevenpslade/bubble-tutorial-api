55c68333e5c8cc23a55a9a358b8fedacada64c02
stevenpslade <stevenpslade@gmail.com>
2018-01-06 18:22:26 -0800

    changing file path for app talking to react app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    render :file => 'public/index.html'
+    puts "hit fallback_index_html"
+    render :file => '/public/index.html'
   end
 end
