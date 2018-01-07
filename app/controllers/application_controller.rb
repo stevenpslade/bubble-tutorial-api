3f4e5d68deab8a1da497fcb1aa6e46fb8b5faaf5
stevenpslade <stevenpslade@gmail.com>
2018-01-06 22:20:06 -0800

    rendering works with plain text, explicitly setting html type now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    # render file: 'public/index.html'
-    render plain: "OK"
+    render file: "public/index.html", content_type: "text/html"
   end
 end
