5f4ca86e42ad2a4cc775bdffd75b08ace4c09bb2
stevenpslade <stevenpslade@gmail.com>
2018-01-06 18:59:10 -0800

    trying change in file rendering format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
     puts "hit fallback_index_html"
-    render :file => '/public/index.html'
+    render file: 'public/index.html'
   end
 end
