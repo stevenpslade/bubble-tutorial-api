64c52bda49b12f2ae5a25c885d03fc66b5fa3fd1
stevenpslade <stevenpslade@gmail.com>
2018-01-09 08:02:45 -0800

    including MimeResponds to get access to the respond_to method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  
   def fallback_index_html
     respond_to do |format|
       format.html { render body: Rails.root.join('public/index.html').read }
