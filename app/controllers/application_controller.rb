2f5e9b046ee275fb02b24dca8397c5dc594cb06e
stevenpslade <stevenpslade@gmail.com>
2018-01-08 22:09:55 -0800

    fixing weird quotes from copy and paste, oops

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
     #render file: "public/index.html"
-    render file: Rails.root.join(“public/index.html”)
+    render file: Rails.root.join("public/index.html")
   end
 end
