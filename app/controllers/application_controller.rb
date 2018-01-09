3437109c179b58ea27fd9c66d2562a22bcbc6849
stevenpslade <stevenpslade@gmail.com>
2018-01-09 07:48:15 -0800

    trying to render html file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   include ActionController::Rendering
 
   def fallback_index_html
-    render file: "public/index.html"
+    render file: "public/index.html", layout: true
   end
 end
