e8d2bef3fd00b97f1b0d91e7aa54712fa7235ea7
stevenpslade <stevenpslade@gmail.com>
2018-01-26 09:26:50 -0800

    adding submenu to display additional action buttons

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import UserStore from './stores/UserStore'
-import { Menu, Icon, Button } from 'semantic-ui-react'
+import { Container, Menu, Icon, Button, Dropdown } from 'semantic-ui-react'
 
 class Header extends Component {
 
@@ -22,12 +22,32 @@ class Header extends Component {
     return links;
   }
 
+  displaySubMenu() {
+    let subMenu = null;
+
+    if (UserStore.isLoggedIn()) {
+      subMenu = (
+        <Menu fixed='top' style={{ top: '7.5%', background: 'rgba(255, 255, 255, 0.65)' }}>
+          <Container text>
+            <Menu.Item as='a' header><Icon size='large' color='pink' name='add circle' /> Add</Menu.Item>
+            <Menu.Item as='a' header position='right' style={{ borderRight: '1px solid rgba(34,36,38,.1)' }}><Icon size='large' color='pink' name='code' /> www.example.com</Menu.Item>
+          </Container>
+        </Menu>
+      );
+    }
+
+    return subMenu;
+  }
+
   render() {
     return (
-      <Menu color='pink' inverted borderless fixed='top'>
-        <Menu.Item className='headerBrandText' header as='h3'>SimpleBubble</Menu.Item>
-        {this.loggedInDependentLinks()}
-      </Menu>
+      <div>
+        <Menu color='pink' inverted borderless fixed='top'>
+          <Menu.Item className='headerBrandText' header as='h3'>SimpleBubble</Menu.Item>
+          {this.loggedInDependentLinks()}
+        </Menu>
+        {this.displaySubMenu()}
+      </div>
     );
   }
 }
