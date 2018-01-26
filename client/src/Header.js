d2d6cdbaed4de767ed4015b2e3671e128922440c
stevenpslade <stevenpslade@gmail.com>
2018-01-25 23:06:34 -0800

    adding sign out button style

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import UserStore from './stores/UserStore'
-import { Menu, Icon } from 'semantic-ui-react'
+import { Menu, Icon, Button } from 'semantic-ui-react'
 
 class Header extends Component {
 
@@ -13,8 +13,9 @@ class Header extends Component {
     let links = [];
     if (UserStore.isLoggedIn()) {
       links = [
-        <li key="sign-out"><button onClick={this.logout}>Sign Out</button></li>,
-        <li key="dashboard"><Link to='/dashboard'>Dashboard</Link></li>
+        <Menu.Item position='right'>
+          <Button inverted onClick={this.logout}>Sign Out</Button>
+        </Menu.Item>
       ];
     }
 
@@ -23,8 +24,9 @@ class Header extends Component {
 
   render() {
     return (
-      <Menu color='pink' inverted size='massive' borderless fixed='top'>
-        <Menu.Item className='headerBrandText' header>SimpleBubble</Menu.Item>
+      <Menu color='pink' inverted borderless fixed='top'>
+        <Menu.Item className='headerBrandText' header as='h3'>SimpleBubble</Menu.Item>
+        {this.loggedInDependentLinks()}
       </Menu>
     );
   }
